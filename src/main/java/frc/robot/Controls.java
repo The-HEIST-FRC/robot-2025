cb1c57ed02adcc47c056a268c7ebbb824f020d08
TheAdmin <theadmin@fedora.attlocal.net>
2025-03-18 10:26:55 -0400

    Initial

---
+++ src/main/java/frc/robot/Controls.java
@@ -0,0 +1,58 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.CANRollerSubsystem.RollerConstants;;
+
+public class Controls {
+    public static final class ControlConstants {
+      public static final int DRIVER_CONTROLLER_PORT = 0;
+      public static final int OPERATOR_CONTROLLER_PORT = 1;
+    }
+
+    // The driver's controller
+    private static final CommandXboxController driverController = new CommandXboxController(
+        ControlConstants.DRIVER_CONTROLLER_PORT);
+  
+    // The operator's controller
+    private static final CommandXboxController operatorController = new CommandXboxController(
+        ControlConstants.OPERATOR_CONTROLLER_PORT);
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  public static void configureBindings(RobotContainer bot) {
+    // Set the A button to run the "runRoller" command from the factory with a fixed
+    // value ejecting the gamepiece while the button is held
+    driverController.rightBumper()
+        .whileTrue(bot.rollerSubsystem.runRoller(bot.rollerSubsystem, () -> RollerConstants.ROLLER_EJECT_VALUE, () -> 0));
+
+    // Set the default command for the drive subsystem to the command provided by
+    // factory with the values provided by the joystick axes on the driver
+    // controller. The Y axis of the controller is inverted so that pushing the
+    // stick away from you (a negative value) drives the robot forwards (a positive
+    // value)
+    bot.driveSubsystem.setDefaultCommand(
+        bot.driveSubsystem.driveArcade(
+            bot.driveSubsystem, () -> -driverController.getLeftY(), () -> -driverController.getRightX(), () -> (driverController.leftBumper().getAsBoolean() ? 0.5 : 1)));
+    // Set the default command for the roller subsystem to the command from the
+    // factory with the values provided by the triggers on the operator controller
+    bot.rollerSubsystem.setDefaultCommand(
+        bot.rollerSubsystem.runRoller(
+            bot.rollerSubsystem,
+            () -> driverController.getRightTriggerAxis(),
+            () -> driverController.getLeftTriggerAxis()));
+  }
+    
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.Autos;
+import frc.robot.subsystems.CANDriveSubsystem;
+import frc.robot.subsystems.CANRollerSubsystem;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems
+  public final CANDriveSubsystem driveSubsystem = new CANDriveSubsystem();
+  public final CANRollerSubsystem rollerSubsystem = new CANRollerSubsystem();
+
+  // The autonomous chooser
+  private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    Controls.configureBindings(this);
+
+    // Set the options to show up in the Dashboard for selecting auto modes. If you
+    // add additional auto modes you can add additional lines here with
+    // autoChooser.addOption
+    autoChooser.setDefaultOption("Autonomous", Autos.exampleAuto(driveSubsystem));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return autoChooser.getSelected();
+  }
+}
